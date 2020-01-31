dc6f72859fa43db7527783081eacb4fb86a487e7
Daniel Bonifacio Romera <dbromera@gmail.com>
2020-01-31 15:52:02 -0300

    cors

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ let http = require('http'),
 let app = http.createServer(function (req, res) {
     res.setHeader("Content-Type", "application/json");
 
+    // Set CORS headers
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Request-Method', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+
     try {
         let data = chartController.getChartData(req.url);
         return res.end(data);
